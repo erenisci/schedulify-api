6101579688731b34c277682b3d8f1d36ec5cb282
Eren İşci <isci.eren@gmail.com>
2024-09-21 00:23:49 +0300

    reset acts

--- src/cronJobs/resetCompletedActivities.ts
+++ src/cronJobs/resetCompletedActivities.ts
@@ -8,15 +8,21 @@ const resetCompletedActivities = () => {
   cron.schedule('0 * * * *', async () => {
     const users = await User.find();
 
+    let activitiesReset = false,
+      timeZone = '';
     for (const user of users) {
       const userTimeZone = user.timeZone || 'UTC';
       const nowInUserTimeZone = moment.tz(userTimeZone).format('HH:mm');
 
       if (nowInUserTimeZone === '00:00') {
-        await Activity.updateMany({ user: user._id }, { isCompleted: false });
-        console.log(`Activities have been reset for the user in the ${userTimeZone} timezone.`);
+        await Activity.updateMany({ userId: user._id }, { isCompleted: false });
+        activitiesReset = true;
+        timeZone = userTimeZone;
       }
     }
+
+    if (activitiesReset)
+      console.log(`Activities have been reset for the user in the ${timeZone} timezone.`);
   });
 };
 
